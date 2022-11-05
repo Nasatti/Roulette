ec1b7d7ae24e291e121b751970bdacb5580e3113
Nasatti <riccardonasatti@gmail.com>
2022-11-05 17:28:12 +0100

    Fine parte grafica client

---
+++ Roulette_Client/Roulette/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Roulette
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            panel_fish1.BackColor = Color.Red;
+            panel_fish5.BackColor = Color.Transparent;
+            panel_fish10.BackColor = Color.Transparent;
+            panel_fish25.BackColor = Color.Transparent;
+            panel_fish50.BackColor = Color.Transparent;
+            panel_fish100.BackColor = Color.Transparent;
+        }
+
+        private void fish5_Click(object sender, EventArgs e)
+        {
+            panel_fish5.BackColor = Color.Red;
+            panel_fish1.BackColor = Color.Transparent;
+            panel_fish10.BackColor = Color.Transparent;
+            panel_fish25.BackColor = Color.Transparent;
+            panel_fish50.BackColor = Color.Transparent;
+            panel_fish100.BackColor = Color.Transparent;
+        }
+
+        private void fish10_Click(object sender, EventArgs e)
+        {
+            panel_fish10.BackColor = Color.Red;
+            panel_fish1.BackColor = Color.Transparent;
+            panel_fish5.BackColor = Color.Transparent;
+            panel_fish25.BackColor = Color.Transparent;
+            panel_fish50.BackColor = Color.Transparent;
+            panel_fish100.BackColor = Color.Transparent;
+        }
+
+        private void fish25_Click(object sender, EventArgs e)
+        {
+            panel_fish25.BackColor = Color.Red;
+            panel_fish1.BackColor = Color.Transparent;
+            panel_fish5.BackColor = Color.Transparent;
+            panel_fish10.BackColor = Color.Transparent;
+            panel_fish50.BackColor = Color.Transparent;
+            panel_fish100.BackColor = Color.Transparent;
+        }
+
+        private void fish50_Click(object sender, EventArgs e)
+        {
+            panel_fish50.BackColor = Color.Red;
+            panel_fish1.BackColor = Color.Transparent;
+            panel_fish5.BackColor = Color.Transparent;
+            panel_fish10.BackColor = Color.Transparent;
+            panel_fish25.BackColor = Color.Transparent;
+            panel_fish100.BackColor = Color.Transparent;
+        }
+
+        private void fish100_Click(object sender, EventArgs e)
+        {
+            panel_fish100.BackColor = Color.Red;
+            panel_fish1.BackColor = Color.Transparent;
+            panel_fish5.BackColor = Color.Transparent;
+            panel_fish10.BackColor = Color.Transparent;
+            panel_fish25.BackColor = Color.Transparent;
+            panel_fish50.BackColor = Color.Transparent;
+        }
+    }
+}
--- Roulette_Client/Roulette/Program.cs
+++ Roulette_Client/Roulette/Program.cs
@@ -16,7 +16,7 @@ namespace Roulette
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Client());
         }
     }
 }
