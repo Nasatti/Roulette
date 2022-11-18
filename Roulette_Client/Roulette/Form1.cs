2e726b77046008d7d3855ce19b13bbf47debed7d
Nasatti <nasatti.riccardo.studente@itispaleocapa.it>
2022-11-18 13:39:32 +0100

    inizio multiclient client

--- Roulette_Client/Roulette/Form1.cs
+++ Roulette_Client/Roulette/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Roulette
 {
@@ -177,6 +179,7 @@ namespace Roulette
         {
             nfish = 0;
             f = 1;
+            StartClient();
         }
         public void ricarica(string s)
         {
@@ -439,6 +442,69 @@ namespace Roulette
             Button_Fish(bfila3, "fila3", e);
         }
 
+        public static void StartClient()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+            int count = 0;
 
+            // Connect to a remote device.  
+            try
+            {
+                string data = "";
+                IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+                Random stringa_casuale = new Random();
+                string stringa_da_inviare = "";
+
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    while (data != "Quit$")
+                    {
+                        stringa_da_inviare = "Messaggio di prova$";
+                        if (stringa_casuale.Next(0, 10) > 8 && count > 15)
+                            stringa_da_inviare = "Quit$";
+                        byte[] msg = Encoding.ASCII.GetBytes(stringa_da_inviare);              //("This is a test<EOF>");
+
+                        int bytesSent = sender.Send(msg);
+                        data = "";
+                        while (data.IndexOf("$") == -1)
+                        {
+                            int bytesRec = sender.Receive(bytes);
+                            data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        }
+                        Console.WriteLine("Messaggio ricevuto: " + data);
+                        System.Threading.Thread.Sleep(1000);
+                        count++;
+                    }
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
     }
 }
